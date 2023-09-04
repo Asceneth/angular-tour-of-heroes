7c02d5dd0c8fd2376fc63afb7a8d9b215d121d41
AscenethHelena <desarrollador2@arsuarez.com>
2023-09-04 09:56:55 -0500

    routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,20 +12,22 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 
   selectedHero?: Hero;
-  onSelect(hero:Hero): void {
-    this.selectedHero = hero;
-  }
 
-  constructor(private HeroService: HeroService) { }
+  constructor(private HeroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+  onSelect(hero:Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   // getHeroes(): void {
